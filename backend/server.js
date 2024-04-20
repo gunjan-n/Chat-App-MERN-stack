0576a87d4bf242c3802f831830480f031c1f3328
Gunjan Naraniya <gnnaraniya@gmail.com>
2024-04-20 15:34:40 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+
+dotenv.config(); 
+
+const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!!");
+// });
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server running on port ${PORT}`)
+});
